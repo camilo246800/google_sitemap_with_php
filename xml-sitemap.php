47077c1709fc94351c73806ac8bdbddb29877d23
Camilo Camargo <camargo246800@gmail.com>
2020-05-23 23:01:44 -0500

    Update xml-sitemap.php

--- xml-sitemap.php
+++ xml-sitemap.php
@@ -2,12 +2,10 @@
 
 /**
  * XML Sitemap PHP Script
- * For more info, see: https
+ * For more info, see: https://concamilo.com/google-sitemap-con-php-y-xls-como-crear-un-mapa-de-tu-sitio-web/
  */
 
 require './config.php';
-require_once '/var/www/html/app/Autoload.php';
-$sitemap = new ControllerSitemap();
 
 // Encabezado para que los navegadores se muestren correctamente, con o sin XSL.
 header( 'Content-Type: application/xml' );
@@ -17,22 +15,27 @@ echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 if ( isset( $xsl ) && !empty( $xsl ) )
 	echo '<?xml-stylesheet type="text/xsl" href="' . SITEMAP_DIR_URL . $xsl . '"?>' . "\n";
 
-//codificar urls
-/*rawurlencode( $file ); ?></loc>*/
 
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-	foreach($sitemap->programsData() as $profile):
+$data = array( 
+       array('url' => '#', 'fecha' => '23-05-20 00:00'),
+       array('url' => '#', 'fecha' => '23-05-20 00:00'),
+       array('url' => '#', 'fecha' => '23-05-20 00:00'),
+       array('url' => '#', 'fecha' => '23-05-20 00:00')
+    );
+    
+foreach($data as $item):
 
-		$date = new DateTime($profile['lastdate']);
-	  
+		$date = new DateTime($item['fecha']);
+
 	        echo '<url>';
-				echo "<loc>{$profile['profile']}</loc>";
-			    echo "<lastmod>{$date->format('Y-m-d')}</lastmod>";
-			    echo "<changefreq>{$freq}</changefreq>";
-			    echo "<priority>{$prio}</priority>";
-			echo '</url>';
+		echo "<loc>{$item['url']}</loc>";
+		echo "<lastmod>{$date->format('Y-m-d')}</lastmod>";
+	        echo "<changefreq>{$freq}</changefreq>";
+		echo "<priority>{$prio}</priority>";
+		echo '</url>';
 
-	endforeach;
+endforeach;
 
 echo '</urlset>';
